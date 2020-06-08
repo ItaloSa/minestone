116e7aab542de507d924f6bd142dbe09a87970ef
Italo <italusousa@gmail.com>
2020-06-08 16:45:08 -0300

    setup gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `MineStone`,
+    description: `A legendary server`,
+    author: `@italosa`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `minestone-website`,
+        short_name: `minestone`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
